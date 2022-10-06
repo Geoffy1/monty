221bca63a6892ebb6345aeeaa4ea13fb68a89223
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2022-10-06 20:15:58 +0300

    Create nop.c
    implement the lazy nop opcode

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - nothing here to see
+ * @stack: no stack operated
+ * @line_cnt : lines given not counted
+ *
+ * Return: NOTHING
+ */
+void nop(stack_t **stack, unsigned int line_cnt)
+{
+	(void) stack;
+	(void) line_cnt;
+}
