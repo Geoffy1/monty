59bb216819b4fcc3100c9e19c3001b8f910cfd6c
Edoh Kindness Ibrahim <ibrahimedoh8@gmail.com>
2022-10-06 22:46:53 +0100

    Update subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 /**
  * _sub -  substracts the first two nodes of the stack
  * @stack: stack given by main
- * @line_cnt: line counter
+ * @line_count: line counter
  *
  * Return: void
  */
-void _sub(stack_t **stack, unsigned int line_cnt)
+void _sub(stack_t **stack, unsigned int line_count)
 {
 	int result;
 	if (!stack || !*stack || !((*stack)->next))
