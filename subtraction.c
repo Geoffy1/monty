a77c592e688565acb14cc23acf9f49ffb0bb9374
Edoh Kindness Ibrahim <ibrahimedoh8@gmail.com>
2022-10-06 23:00:54 +0100

    Update subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -11,10 +11,10 @@ void _sub(stack_t **stack, unsigned int line_count)
 	int result;
 	if (!stack || !*stack || !((*stack)->next))
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_cnt);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_count);
 		exit(EXIT_FAILURE);
 	}
 	result = ((*stack)->next->n) - ((*stack)->n);
-	pop(stack, line_cnt); /*For top node*/
+	pop(stack, line_count); /*For top node*/
 	(*stack)->n = result;
 }
