51be50941d814f9f923d79ae91b3f494f8a69002
Edoh Kindness Ibrahim <ibrahimedoh8@gmail.com>
2022-10-06 22:32:17 +0100

    Update add.c

--- add.c
+++ add.c
@@ -1,24 +1,21 @@
 #include "monty.h"
 
 /**
- * m_add - add the top two elements of the stack
+ * _add - add the top two elements of the stack
  * @stack: double pointer tot he beginning of the stack
- * @line_number: script line number
+ * @line_count: script line number
  *
  * Return: void
  */
 void _add(stack_t **stack, unsigned int line_count)
-{
-	int n = 0;
-
-	if (var.stack_len < 2)
 	{
-		dprintf(STDOUT_FILENO,
-			"L%u: can't add, stack too short\n",
-			line_number);
+	int result;
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
 	}
-	n += (*stack)->n;
-	m_pop(stack, line_number);
-	(*stack)->n += n;
+	result = ((*stack)->next->n) + ((*stack)->n);
+	pop(stack, line_cnt); /* For top node */
+	(*stack)->n = result;
 }
