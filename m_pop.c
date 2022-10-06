f0292431dd9f316ba3eaa248d7b2419a9c4da131
Edoh Kindness Ibrahim <ibrahimedoh8@gmail.com>
2022-10-06 23:06:55 +0100

    Update m_pop.c

--- m_pop.c
+++ m_pop.c
@@ -11,7 +11,7 @@ void pop(stack_t **stack, unsigned int line_count)
 	stack_t *tmp = NULL;
 	if (!stack || !*stack)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_cnt);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_count);
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*stack)->next;
