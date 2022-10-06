aa4c1fe6d8ca35c37c048919c74ccf32fc610531
Edoh Kindness Ibrahim <ibrahimedoh8@gmail.com>
2022-10-06 17:08:40 +0100

    Update push.c
    updated push

--- push.c
+++ push.c
@@ -1,4 +1,25 @@
 #include "monty.h"
+#include <ctype.h>
+
+/**
+ * check_for_digit - checks that a string only contains digits
+ * @arg: string to check
+ *
+ * Return: 0 if only digits, else 1
+ */
+static int check_for_digit(char *arg)
+{
+	int i;
+
+	for (i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && i == 0)
+			continue;
+		if (isdigit(arg[i]) == 0)
+			return (1);
+	}
+	return (0);
+}
 
 /**
  * m_push - push an integer onto the stack
