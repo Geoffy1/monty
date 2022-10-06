0fa36ffdc26c3f7e433209c49ab48a2bafb040a7
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2022-10-07 01:24:22 +0300

    Create monty.h

--- monty.h
+++ monty.h
@@ -33,22 +33,6 @@
 		}                     \
 	}
 
-/**
- * struct var_s - struct to contain the main variables of the Monty interpreter
- * @queue: flag to determine if in stack vs queue mode
- * @stack_len: length of the stack
- */
-typedef struct var_s
-{
-	int queue;
-	size_t stack_len;
-} var_t;
-
-#define STACK 0
-#define QUEUE 1
-
-/* global struct to hold flag for queue and stack length */
-extern var_t var;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -77,7 +61,7 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_count);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
@@ -93,8 +77,6 @@ typedef struct help
 	char *argument;
 } help;
 help global;
-int is_digit(char *string);
-int isnumber(char *str);
 
 stack_t *add_node(stack_t **stack, const int n);
 stack_t *queue_node(stack_t **stack, const int n);
@@ -108,19 +90,25 @@ void pop(stack_t **stack, unsigned int line_count);
 void nop(stack_t **stack, unsigned int line_count);
 void swap(stack_t **stack, unsigned int line_count);
 
-void m_add(stack_t **stack, unsigned int line_count);
-void m_sub(stack_t **stack, unsigned int line_count);
-void m_mul(stack_t **stack, unsigned int line_count);
-void m_div(stack_t **stack, unsigned int line_count);
-void m_mod(stack_t **stack, unsigned int line_count);
+void _add(stack_t **stack, unsigned int line_count);
+void _sub(stack_t **stack, unsigned int line_count);
+void _mul(stack_t **stack, unsigned int line_count);
+void _div(stack_t **stack, unsigned int line_count);
+void mod(stack_t **stack, unsigned int line_count);
+
+void pchar(stack_t **stack, unsigned int line_count);
+
 
 void rotl(stack_t **stack, unsigned int line_count);
 void rotr(stack_t **stack, unsigned int line_count);
-void m_stack(stack_t **stack, unsigned int line_count);
-void m_queue(stack_t **stack, unsigned int line_count);
-void m_pstr(stack_t **stack, unsigned int line_count);
+void stack(stack_t **stack, unsigned int line_count);
+void queue(stack_t **stack, unsigned int line_count);
+void pstr(stack_t **stack, unsigned int line_count);
 void m_fs_close(int status, void *arg);
 void free_lineptr(int status, void *arg);
+
+int is_digit(char *string);
+int isnumber(char *str);
 
 void opcode(stack_t **stack, char *str, unsigned int line_count);
 
